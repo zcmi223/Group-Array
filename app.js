b0d0b48ce4f78aa3e38259deb92b9bdaacbd7f95
Zachary Middleton <zmidd24@gmail.com>
2022-03-18 12:14:27 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const closedface = document.querySelector('.closed')
+const openface = document.querySelector('.open')
+
+// Add event listener
+closedface.addEventListener('click', () => {
+    if(openface.classList.contains('open')) {
+        openface.classList.add('active');
+        closedface.classList.remove('active');
+    }
+});
+
+openface.addEventListener('click', () => {
+    if(closedface.classList.contains('closed')) {
+        closedface.classList.add('active');
+        openface.classList.remove('active');
+    }
+});
